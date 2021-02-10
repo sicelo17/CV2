b8e28ea2e466828178bd4402bf845ec6d771700f
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-02-10 12:25:36 +0200

    function not working

--- data.js
+++ data.js
@@ -1,30 +1,31 @@
-const servicesData = [
-    {
-        name: "Web Development",
-        desc: "Services that I offer and are very passionate about",
-        icon: "bx bx-laptop services_icon"
-    },
-    {
-        name: "UI/UX Design",
-        desc: "Services that I offer and are very passionate about",
-        icon: "bx bx-palette services_icon"
-    }
-];
 
+function change_myselect(sel) {
 
-// function to display data in services
-const serviceTemplate = (service) => {
-    return `
-         <div class="services_data">
-            <i class="${service.icon}"></i>
-            <h3 class="services_title">${service.name}</h3>
-            <p class="services_description">Services that I offer and are very passionate about</p>
-            <a href="#" class="button">Know More</a>
-        </div>
-    `
+    
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            servicesData = JSON.parse(this.responseText);
+            // function to display data in services
+            const serviceTemplate = (service) => {
+                return `
+                    <div class="services_data">
+                        <i class="${service.icon}"></i>
+                        <h3 class="services_title">${service.name}</h3>
+                        <p class="services_description">Services that I offer and are very passionate about</p>
+                        <a href="#" class="button">Know More</a>
+                    </div>
+                `
+                };
+
+            // adding the html to the page
+            document.getElementById("serv").innerHTML = `
+            ${servicesData.map(serviceTemplate).join('')}
+            `;
+
+            }
+        };
+    xmlhttp.open("GET", "constants.json", true);
+    
+    xmlhttp.send();
 }
-
-// adding the html to the page
-document.getElementById("serv").innerHTML = `
-    ${servicesData.map(serviceTemplate).join('')}
-`;
